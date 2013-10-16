e03b428a533984335cfb24d766a630e5d147adee
Riccardo Tacconi <Riccardo.Tacconi@wonga.com>
2013-10-16 15:02:33 +0100

    update Pantry

--- pantry_jenkins_slave_deleted_event_handler/pantry_jenkins_slave_deleted_event_handler.rb
+++ pantry_jenkins_slave_deleted_event_handler/pantry_jenkins_slave_deleted_event_handler.rb
@@ -1,12 +1,15 @@
 module Wonga
   module Daemon
     class PantryJenkinsSlaveDeletedEventHandler
-      def initialize(config)
-        @config = config
+      def initialize(api_client, logger)
+        @logger = logger
+        @api_client = api_client
       end
 
       def handle_message(message)
-        #Put message handling code in here
+        @logger.info "Updating deleted status for Request:#{message['pantry_request_id']}, Name:#{message['instance_name']}, InstanceID:#{message['instance_id']}"
+        @api_client.send_put_request("/api/ec2_instances/#{message['id']}", { terminated: true, instance_id: message['instance_id'] })
+        @logger.info "Updating deleted joined status for Request:#{message['pantry_request_id']} succeeded"
       end
     end
   end
--- spec/pantry_jenkins_slave_deleted_event_handler/pantry_jenkins_slave_deleted_event_handler_spec.rb
+++ spec/pantry_jenkins_slave_deleted_event_handler/pantry_jenkins_slave_deleted_event_handler_spec.rb
@@ -2,9 +2,16 @@ require 'spec_helper'
 require_relative '../../pantry_jenkins_slave_deleted_event_handler/pantry_jenkins_slave_deleted_event_handler'
 
 describe Wonga::Daemon::PantryJenkinsSlaveDeletedEventHandler do
+  let(:message) { {"node" => "jenkins-linux-agent.vagrant", "server_ip" => "127.0.0.1", "server_port" => "8080", 'id' => "443444"} }
+  let(:api_client) { instance_double('Wonga::Daemon::PantryApiClient').as_null_object }
+  let(:logger) { instance_double('Logger').as_null_object }
+  subject { described_class.new(api_client, logger) }
+
   it_behaves_like 'handler'
   describe "#handle_message" do
-    #add in unit test code for handle message here.
+    it "sends post request" do
+      subject.handle_message(message)
+      expect(api_client).to have_received(:send_put_request).with("/api/ec2_instances/#{message['id']}", {:terminated => true, :instance_id => nil})
+    end
   end
 end
-
