029e83e7877f1f713bca35d40f098b30b5e7fea8
Riccardo Tacconi <Riccardo.Tacconi@wonga.com>
2013-10-16 12:58:58 +0100

    first commit

---
+++ spec/pantry_jenkins_slave_deleted_event_handler/pantry_jenkins_slave_deleted_event_handler_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require_relative '../../pantry_jenkins_slave_deleted_event_handler/pantry_jenkins_slave_deleted_event_handler'
+
+describe Wonga::Daemon::PantryJenkinsSlaveDeletedEventHandler do
+  it_behaves_like 'handler'
+  describe "#handle_message" do
+    #add in unit test code for handle message here.
+  end
+end
+
