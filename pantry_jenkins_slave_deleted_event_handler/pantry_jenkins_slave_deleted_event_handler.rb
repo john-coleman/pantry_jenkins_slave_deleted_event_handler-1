712072112611e74499e7eb6a15271ce7b1a086af
Riccardo Tacconi <Riccardo.Tacconi@wonga.com>
2013-10-17 18:11:12 +0100

    TD-987

---
+++ pantry_jenkins_slave_deleted_event_handler/pantry_jenkins_slave_deleted_event_handler.rb
@@ -0,0 +1,16 @@
+module Wonga
+  module Daemon
+    class PantryJenkinsSlaveDeletedEventHandler
+      def initialize(api_client, logger)
+        @logger = logger
+        @api_client = api_client
+        @publisher = publisher
+      end
+
+      def handle_message(message)
+        @logger.info "Updating deleted status for Jenkins slave: #{message['server_ip']}"
+        @api_client.send_put_request("/api/jenkins_slaves/#{message['id']}", { removed: true })
+      end
+    end
+  end
+end
